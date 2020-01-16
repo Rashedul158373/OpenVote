fc7d6286ce6cdcd1cd64e8a6f38f37bc241748b9
RASHEDUL <rashedulislam158373@gmail.com>
2020-01-16 17:16:08 +0600

    splash screen complete

--- app/src/main/java/com/example/openvote/SplashActivity.java
+++ app/src/main/java/com/example/openvote/SplashActivity.java
@@ -19,6 +19,7 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_splash);
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);

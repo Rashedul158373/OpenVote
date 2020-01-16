c5594ce7d3b5d160038bd528d0481d2a5f504b1e
RASHEDUL <rashedulislam158373@gmail.com>
2020-01-16 13:38:07 +0600

    splash screen complete

--- app/src/main/java/com/example/openvote/SplashActivity.java
+++ app/src/main/java/com/example/openvote/SplashActivity.java
@@ -5,16 +5,33 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.WindowManager;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import com.example.openvote.pojo.ProgressBarAnimation;
 
 public class SplashActivity extends AppCompatActivity {
+    private TextView textView;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        Handler myHandler = new Handler();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        textView = findViewById(R.id.textView);
+        progressBar = findViewById(R.id.progressBar);
 
+
+        progressBar.setMax(100);
+        progressBar.setScaleY(3f);
+
+        progressAnimation();
+
+        Handler myHandler = new Handler();
         myHandler.postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -25,5 +42,14 @@ public class SplashActivity extends AppCompatActivity {
 
             }
         }, 3000);
+
     }
+    private void progressAnimation() {
+        ProgressBarAnimation anim = new ProgressBarAnimation(this, progressBar, textView, 0f, 100f);
+        anim.setDuration(3000);
+        progressBar.setAnimation(anim);
+
+    }
+
+
 }
